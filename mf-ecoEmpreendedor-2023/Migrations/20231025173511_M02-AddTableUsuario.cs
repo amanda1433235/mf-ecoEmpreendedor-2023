188d36dca4e40ac5f0420c0b1862af05fb421e3b
amanda1433235 <amanda.gomes.1433235@sga.pucminas.br>
2023-10-25 14:47:06 -0300

    Configuração do Banco de dados e da tabela Usuário

---
+++ mf-ecoEmpreendedor-2023/Migrations/20231025173511_M02-AddTableUsuario.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_ecoEmpreendedor_2023.Migrations
+{
+    public partial class M02AddTableUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNasc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rg = table.Column<int>(type: "int", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ mf-ecoEmpreendedor-2023/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_ecoEmpreendedor_2023.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data de nascimento!")]
+        public string DataNasc { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o endereço!")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o login!")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha!")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o telefone!")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o RG!")]
+        public int Rg { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o CPF!")]
+        public string Cpf { get; set; }
+
+
+
+    }
+}
